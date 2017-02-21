c427c6f938ef36020c8a090254ef9a1851fc03d7
benrau87 <benrau87@users.noreply.github.com>
2017-02-21 13:07:55 -0600

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit 1
+fi
+
+apt-get install xrdp -y
+apt-get update
+apt-get install mate-core mate-desktop-environment mate-notification-daemon
+
+sed -i.bak '/fi/a #xrdp multiple users configuration \n mate-session \n' /etc/xrdp/startwm.sh
+
+# Set keyboard layout in xrdp sessions 
+cd /etc/xrdp 
+test=$(setxkbmap -query | awk -F":" '/layout/ {print $2}') 
+echo "your current keyboard layout is.." $test
+setxkbmap -layout $test 
+sudo cp /etc/xrdp/km-0409.ini /etc/xrdp/km-0409.ini.bak 
+sudo xrdp-genkeymap km-0409.ini
